ac75546d82ceda483c9c5f6559f7a9f0dca98a69
hobyfrezk <hobyfrezk@gmail.com>
2016-09-06 17:15:31 +0200

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,1 +1,6 @@
-
+var LED = {
+	state =  // 1--> LED on, 0--> Led off 
+	brightness =  //brightness of led, 0 if it is turned off
+	time = // how long the led in this state
+	SwitchTime =  // how much time left for switching led state
+} 
