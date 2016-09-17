4ea6a8ca1fb01ef074722f81ed46bd25d03f988d
hobyfrezk <hobyfrezk@gmail.com>
2016-09-17 12:31:29 +0200

    Create Interactive_mode.py

---
+++ Interactive_mode.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+import time
+ 
+LED = 4
+
+def light_control(requirement = 0):
+    print requirement
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(LED,GPIO.OUT)
+     
+    if requirement ==0: #turn off led
+        GPIO.output(LED, 0)
+         
+    else: # control led brightness according to requirement
+        port4 = GPIO.PWM(LED, 100)
+        port4.start(requirement)
+        time.sleep(5)
+        
+if __name__ == "__main__":
