b5cb262b8a76e94e090fb54b27c48387f89f5fde
hobyfrezk <hobyfrezk@gmail.com>
2016-09-18 10:50:38 +0200

    Update Interactive_mode.py

--- Interactive_mode.py
+++ Interactive_mode.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 import time
+from Automode_light import automode
  
 LED = 4
 
@@ -15,8 +16,25 @@ def light_control(requirement = 0):
     else: # control led brightness according to requirement
         port4 = GPIO.PWM(LED, 100)
         port4.start(requirement)
-        time.sleep(5)
+
         
-if __name__ == "__main__":
-	print "what mode do you want:"
+        
+def interactiveMode():
+	print "Select mode:"
+	print "1. go back to auto mode"
+	print "2. select brightness"
+	print "3. turn off the system"
+	
+	command = raw_input()
+	if str(command) == str(1):
+		automode()
+		
+	if str(command) == str(2):
+		print "Enter the brightness you want"
+		brightness = raw_iuput()
+		light_control(brightness)
+		
+	if str(command) == str(3):
+	
+	
 	
