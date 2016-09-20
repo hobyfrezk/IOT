17815d1d707b8e905f14c93678eeb22739fc041d
hobyfrezk <hobyfrezk@gmail.com>
2016-09-20 17:23:44 +0200

    Update mainserver.py

--- mainserver.py
+++ mainserver.py
@@ -1,8 +1,14 @@
-AUTOMODE = 1
-INTERACTIVE = 2
 from Automode_light import automode 
 from Interactive_mode import interactiveMode
 
+#WIRING SETTING
+LED = 4
+
+
+USER_PRESENCE = 0
+USER_ABSENCE = 1
+AUTOMODE = 2
+INTERACTIVE = 3
 
 # used to read log file in local, we only care about neweast data, so read last number of file
 def read_data(filename):
