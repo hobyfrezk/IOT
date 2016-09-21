f5e9600082baf0401fd142e70a9976cb029cac56
hobyfrezk <hobyfrezk@gmail.com>
2016-09-21 11:51:29 +0200

    Update thingspeak.py

--- thingspeak.py
+++ thingspeak.py
@@ -1,1 +1,23 @@
+from lightsensor import Light
+import httplib, urllib
 
+# field1: user attendence, field2: lightsensor, field3: led working state
+
+def update(datatype, data):
+    if datatype == USER:
+        params = urllib.urlencode({'field1': data, 'key':'NXCLG7W3AQVRDBL1'})
+        
+    if datatype == NATURALLIGHT:
+        params = urllib.urlencode({'field2': data, 'key':'NXCLG7W3AQVRDBL1'})
+    
+    if datatype == LED:
+        params = urllib.urlencode({'field3': data, 'key':'NXCLG7W3AQVRDBL1'})
+        
+    headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
+    
+    conn = httplib.HTTPConnection("api.thingspeak.com:80")
+    conn.request("POST", "/update", params, headers)
+    response = conn.getresponse()
+    print response.status, response.reason
+    data = response.read()
+    conn.close()
