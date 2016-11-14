6b2140c8e94bcc0c6e24990e9bb4a3bf72731e8b
hobyfrezk <hobyfrezk@gmail.com>
2016-11-15 00:07:35 +0100

    Update configfile_maker.py

--- Distributed/configfile_maker.py
+++ Distributed/configfile_maker.py
@@ -1,17 +1,30 @@
+
 import json
 
+
+
 config = {'mqtt': {'ip_address': '192.168.1.178'}, 
+
 	  'thingspeak': {'thingspeak_key': 'NXCLG7W3AQVRDBL1'},
+
 	  'mode': {'Automode': '0', 'Manualmode': '1'},
-	  "sleep_timeout": '900' 
+
+	  'sleep_timeout': '900' ,
+
 	  
+
 	  'zone': ['ZoneA'],
+
 	  'device': {'led': ['led01'],
-		     'PIR': 'PIR01',
-		     'Lightsensor': 'LSensor01'}
-	  'sleep_timeout': '900' 
-	  'lamp_luminance': {'1000':'state1', '600': 'state2', '400':'state3', '100': 'state4'}
+
+		     'PIR': ['PIR01'],
+
+		     'Lightsensor': ['LSensor01']},
+
+	  'lamp_luminance': {'1000':'state1', '600': 'state2', '400':'state3', '100': 'state4'},
+
 	  'lightsensor': {'address': '0x39','control_on': '0x03','control_off': '0x00'}
+
 	 }
 
 
