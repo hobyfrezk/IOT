87a8e0c106a6b74e54fa1e3bba98fa1f17d1ce45
hobyfrezk <hobyfrezk@gmail.com>
2016-11-19 21:43:34 +0100

    Update configfile_maker.py

--- Distributed/configfile_maker.py
+++ Distributed/configfile_maker.py
@@ -1,17 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 16 15:45:33 2016
+
+@author: Wang
+"""
+
 import json
 
-config = {'mqtt': {'ip_address': '192.168.1.178'}, 
+configuration = {'mqtt': {'ip_address': '192.168.1.178'}, 
 	  'thingspeak': {'thingspeak_key': 'NXCLG7W3AQVRDBL1'},
 	  'state_index': {'Off':'0', 'Automode': '1', 'Manualmode': '2'},
 	  'sleep_timeout': '900' ,
 	  'zone': {'Activated': ['ZoneA', 'ZoneB'], 'Disactivated': []},
 	  'device': {'LED': {'ZoneA': 'LED01', 'ZoneB': 'LED02'},
 		     'PIR': {'ZoneA': 'PIR01'},
-		     'Lightsensor': {'ZoneA': 'LSensor01'},
+		     'LSensor': {'ZoneA': 'LSensor01'},
 		     'button': {'ZoneA': 'button01'}},			 
-	  'lamp_luminance': {'1000':'state1', '600': 'state2', '400':'state3', '100': 'state4'},
+	  'lamp_luminance': {'1000':'state1', '600': 'state2', '400':'state3', '100': 'state4', '0': 'state0'},
 	  'lightsensor': {'address': '0x39','control_on': '0x03','control_off': '0x00'}
 	 }
 
 with open('config.json', 'w') as f:
-	json.dump(config, f)
+    json.dump(configuration, f)
